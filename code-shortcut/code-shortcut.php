c80162c7f5aacaeb2c6c36408d7773259d5b069b
Jim Barritt <jim@planet-ix.com>
2010-04-21 15:07:36 +0100

    Added code-shortcut

---
+++ code-shortcut/code-shortcut.php
@@ -0,0 +1,53 @@
+<?php
+/*
+Plugin Name: Code Shortcode
+Plugin URI: http://jimbarritt.com
+Description: Shortcode that generates a code block for google syntax highlighter
+Version: 0.0
+Author: Jim Barritt
+Author URI: http://jimbarritt.com
+*/
+
+function openCodeTag($language) {
+	return "<pre name='code' class='".$language.":nogutter:nocontrols'>";
+}
+
+function escapeCode($content) {        
+    $str = str_replace('<br','',$content); 
+    $str = str_replace('<','&lt;',$content);
+    $str = str_replace('>','&gt;',$content);
+    return $str;
+}
+
+
+function closeCodeTag() {
+	return "</div>";
+}
+
+// [code]some code goes here[/code] -> to nicely formatted code
+function shortcode_java($attr, $content) {
+    return openCodeTag('java').strip_tags($content).closeCodeTag();
+}
+add_shortcode('java', 'shortcode_java');
+
+function shortcode_php($attr, $content) {
+    return openCodeTag('php').strip_tags($content).closeCodeTag();
+}
+add_shortcode('php', 'shortcode_php');
+
+function shortcode_html($attr, $content) {
+    return openCodeTag('html').$content.closeCodeTag();
+}
+add_shortcode('html', 'shortcode_html');
+
+function shortcode_bash($attr, $content) {
+    return openCodeTag('java').$content.closeCodeTag();
+}
+add_shortcode('bash', 'shortcode_bash');
+
+function shortcode_xml($attr, $content) {
+    return openCodeTag('xml').$content.closeCodeTag();
+}
+add_shortcode('xml', 'shortcode_xml');
+
+?>
